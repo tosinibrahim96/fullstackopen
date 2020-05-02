07408137b25315bce1e3901a94f7b7df914cdbae
Ibrahim Alausa <tosinibrahim96@gmail.com>
2020-05-02 10:11:07 +0100

    complete feature for delete and update contact details

--- part2/phonebook/src/components/ContactInfo.jsx
+++ part2/phonebook/src/components/ContactInfo.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const ContactInfo = ({ name,number }) => {
+const ContactInfo = ({ name, number, handleDelete, id }) => {
   return (
-     <li>{`${name}  : ${number}`}</li>
+    <li>
+      {`${name}  : ${number}`}{' '}
+      <button onClick={() => handleDelete(id)}>Delete</button>
+    </li>
   );
 };
 
